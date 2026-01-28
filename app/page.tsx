4930822a3dfc9ce040d48516a1af3d6092276dd9
Vuanjun <victor@meepshop.com>
2026-01-29 00:04:04 +0800

    no ads

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { LanguageProvider } from "@/components/language-provider"
 import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
-import { AdBanner } from "@/components/ad-banner"
+// import { AdBanner } from "@/components/ad-banner"
 import { FeaturedContent } from "@/components/featured-content"
 import { BrowseByMood } from "@/components/browse-by-mood"
 import { About } from "@/components/about"
@@ -19,13 +19,12 @@ export default function Home() {
         <main className="flex-1">
           <Hero />
 
-          {/* Ad Banner - Top */}
-          <AdBanner />
+          {/* 廣告區塊暫未使用，日後有再開啟 */}
+          {/* <AdBanner /> */}
 
           <FeaturedContent />
 
-          {/* Ad Banner - Between Sections */}
-          <AdBanner />
+          {/* <AdBanner /> */}
 
           <BrowseByMood />
 
